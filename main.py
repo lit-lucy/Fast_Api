a80adf89044173b966b24aaee6e23d6379b7e7de
Lubov Glushanina <litlucyadress@gmail.com>
2023-04-28 10:24:16 -0600

    Added initial requests

---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "Items"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_price": item.price, "item_id": item_id}
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    read_root()
+
